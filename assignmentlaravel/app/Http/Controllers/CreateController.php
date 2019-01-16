ddbb5b168933467e00d108e27948a9c0bb8adeed
Will Piears <willpiears@live.co.uk>
2019-01-16 13:44:40 +0000

    Fix create request to create dvds

--- assignmentlaravel/app/Http/Controllers/CreateController.php
+++ assignmentlaravel/app/Http/Controllers/CreateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Dvd;
 
 class CreateController extends Controller
 {
@@ -10,6 +11,11 @@ class CreateController extends Controller
     public function create()
     {
         //Stub
-        return view('welcome');
+        $dvd = new Dvd();
+        $dvd->title = request('title');
+        $dvd->description = request('description');
+        $dvd->save();
+
+        return redirect('/');;
     }
 }
--- assignmentlaravel/resources/views/home.blade.php
+++ assignmentlaravel/resources/views/home.blade.php
@@ -1,88 +1,56 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+  <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+  <!-- styles -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- scripts -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div>
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Title</th>
+          <th scope="col">Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($dvds as $dvd)
+          <tr>
+          <th scope="row">{{ $dvd->id }}</th>
+          <td>{{ $dvd->title }}</td>
+          <td>{{ $dvd->description }}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+  <div class="container">
+    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Add DVD</button>
+    <div id="demo" class="collapse">
+      <form action="/create" method="POST">
+        {{ csrf_field() }}
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div>
-            <table class="table table-dark">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Title</th>
-                  <th scope="col">Description</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($dvds as $dvd)
-                  <tr>
-                  <th scope="row">{{ $dvd->id }}</th>
-                  <td>{{ $dvd->title }}</td>
-                  <td>{{ $dvd->description }}</td>
-                  </tr>
-                @endforeach 
-              </tbody>
-            </table>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputTitle">Title</label>
+            <input class="form-control" id="inputTitle" name="title" placeholder="Title">
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputDescription">Description</label>
+            <input class="form-control" id="inputDescription" name ="description" placeholder="Description">
+          </div>
         </div>
+        <button type="submit" class="btn btn-primary">Add</button>
+      </form>
+    </div>
+  </div>
     </body>
 </html>
