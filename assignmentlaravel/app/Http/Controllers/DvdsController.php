3d1e12f794e81f5fef9f9da1ee0bed980f58c29e
Will Piears <willpiears@live.co.uk>
2019-01-16 16:00:38 +0000

    Controller functions to view page and update

---
+++ assignmentlaravel/app/Http/Controllers/DvdsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Dvd;
+
+class DvdsController extends Controller
+{
+    //
+    public function update($id)
+    {
+        $dvd = Dvd::find($id);
+
+        $dvd->title = request('title');
+        $dvd->description = request('description');
+
+        $dvd->save();
+
+        return redirect('/');
+    }
+
+    public function edit($id)
+    {
+        $dvd = Dvd::find($id);
+
+        return view('dvds.edit', compact('dvd'));
+    }
+}
